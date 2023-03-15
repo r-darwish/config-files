971acc35ae0b9ba0703ec9da8012c10e679236fc
Roey Darwish Dror <roey.ghost@gmail.com>
2023-03-15 21:48:18 +0200

    Better escape

--- lazyvim/lua/plugins/self.lua
+++ lazyvim/lua/plugins/self.lua
@@ -2,6 +2,12 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "ntpeters/vim-better-whitespace" },
   {
+    "max397574/better-escape.nvim",
+    opts = {
+      mapping = { "jk", "jj" },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",

f67f0055eef5342c71a4e5cf18a5a9c647a8bb29
Roey Darwish Dror <roey.ghost@gmail.com>
2023-03-15 22:17:18 +0200

    Telescope Lua Snippets

--- lazyvim/lua/plugins/telescope.lua
+++ lazyvim/lua/plugins/telescope.lua
@@ -1,13 +1,29 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "benfowler/telescope-luasnip.nvim",
+        config = function()
+          require("telescope").load_extension("luasnip")
+        end,
+      },
+    },
     keys = {
+      {
+        "<C-s>",
+        function()
+          require("telescope").extensions.luasnip.luasnip()
+        end,
+        desc = "LuaSnippets",
+        mode = { "i" },
+      },
       {
         "<leader>sj",
         function()
           require("telescope.builtin").jumplist()
         end,
-        desc = "Jump list",
+        desc = "Jump List",
       },
     },
     opts = {

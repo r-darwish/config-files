75b283b8842c3a2d5f052c51bc3497b9bb94b46c
Roey Darwish Dror <roey.ghost@gmail.com>
2023-11-06 21:38:35 +0200

    Clipedit kitten

---
+++ kitty/clipedit.py
@@ -0,0 +1,37 @@
+import argparse
+import subprocess
+import tempfile
+import os
+
+from typing import List
+from kitty.boss import Boss
+from kittens.tui.loop import debug
+
+
+class Namespace(argparse.Namespace):
+    empty: bool
+
+
+parser = argparse.ArgumentParser(description="Edit clipboard kitten")
+parser.add_argument("-e", "--empty", action="store_true", help="Start with an empty file")
+
+
+def main(args: List[str]) -> str:
+    parsed_args = parser.parse_args(args=args[1:], namespace=Namespace)
+
+    clipboard = ""
+    if not parsed_args.empty:
+        clipboard = subprocess.check_output("/usr/bin/pbpaste", shell=False, encoding="utf-8")
+    with tempfile.NamedTemporaryFile(mode="w+") as f:
+        f.write(clipboard)
+        f.flush()
+        subprocess.check_call([os.environ.get("EDITOR", "/usr/local/bin/hx"), f.name])
+        f.seek(0)
+        new_clipboard = f.read()
+
+    if new_clipboard != clipboard:
+        subprocess.run("/usr/bin/pbcopy", shell=False, input=clipboard, encoding="utf-8", check=True)
+
+
+def handle_result(args: List[str], answer: str, target_window_id: int, boss: Boss) -> None:
+    pass

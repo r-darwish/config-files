0c6b6ce1cb26e57d104a867c37ebda33d277fe74
Roey Darwish Dror <roey.ghost@gmail.com>
2023-12-20 12:14:12 +0200

    Tidy config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-if [[ -f /usr/bin/apt-get ]]; then
-        sudo add-apt-repository ppa:neovim-ppa/stable
-        sudo apt-get update
-        sudo apt-get install neovim fzf tmux zsh ripgrep python3-venv
-        sudo snap install starship
-fi
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
-git clone https://github.com/r-darwish/config-files ~/config-files
-cd ~/config-files
-zsh tmux/install.zsh
 mkdir -p ~/.config
-ln -s ~/config-files/nvim ~/.config/nvim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-rm -f ~/.zshrc
-ln -s ~/config-files/zshrc ~/.zshrc
-~/config-files/install-zsh-plugins.zsh
-nvim +PlugInstall +COQdep
-chsh -s $(which zsh)
+
+link_config() {
+    local source target
+    source="$PWD/$1"
+    target="$HOME/.config/$(basename $1)"
+
+    if link=$(readlink "$target"); then
+        if [[ "$link" == "$source" ]]; then
+            echo "Already linked $target"
+            return 0
+        fi
+    fi
+
+    rm -rf "$target"
+    ln -s "$source" "$target"
+}
+
+for c in config/*; do
+    link_config "$c"
+done
+
+for c in dynamic_config/*; do
+        cp -r "$c" ~/.config
+done
+
+rm -f ~/.zshrc; ln -s "$PWD/zshrc" ~/.zshrc
+#~/config-files/install-zsh-plugins.zsh
--- install-git.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-REPO_PATH="${1:-${HOME}/config-files}"
-
-echo -n "Using ${REPO_PATH} -- Press enter to continue..."; read
-
-mkdir -p ~/.config/git
-ln -sf ${REPO_PATH}/gitconfig ~/.config/git/config

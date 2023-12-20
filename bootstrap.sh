a2d94454054b4d312f31211679f83ef17e9c6d64
Roey Darwish Dror <roey.ghost@gmail.com>
2023-12-20 15:17:20 +0200

    Bootstrap installs brew

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,17 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 mkdir -p ~/.config
 
+if [[ "$(uname)" == "Linux" ]]; then
+    if ! command -v brew &>/dev/null; then
+        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    fi
+fi
+
 link_config() {
     local source target
     source="$PWD/$1"
-    target="$HOME/.config/$(basename $1)"
+    target="$HOME/.config/$(basename "$1")"
 
     if link=$(readlink "$target"); then
         if [[ "$link" == "$source" ]]; then
@@ -26,8 +33,12 @@ for c in config/*; do
 done
 
 for c in dynamic_config/*; do
-        cp -r "$c" ~/.config
+    cp -r "$c" ~/.config
 done
 
-rm -f ~/.zshrc; ln -s "$PWD/zshrc" ~/.zshrc
+rm -f ~/.zshrc
+ln -s "$PWD/zshrc" ~/.zshrc
 git config --global include.path "$PWD/.gitconfig"
+test -f ~/.antidote || git clone --depth=1 https://github.com/mattmc3/antidote.git ~/.antidote
+make -C zsh
+brew bundle install

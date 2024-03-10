ac0b2a1b2f52bba1232dd331c03781fac3dae39c
Roey Darwish Dror <roey.ghost@gmail.com>
2024-03-10 12:47:58 +0200

    Add more packages to sync installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,7 +32,7 @@ for c in zsh/*.txt; do
     antidote bundle <"$c" >"$zsh_file" || true
 done
 if [[ -n "$BACKGROUND" ]]; then
-    brew install starship atuin lsd || true
+    brew install starship atuin lsd zellij yazi fzf || true
     echo "Running package installation in the background"
     nohup brew bundle install >/tmp/brew.log 2>&1 &
 else

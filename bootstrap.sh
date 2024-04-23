7b5a33573ea5ed401797ed9db0f8e5220b743d3b
Roey Darwish Dror <roey.ghost@gmail.com>
2024-04-23 22:49:25 +0300

    aichat integration

--- bootstrap.sh
+++ bootstrap.sh
@@ -58,3 +58,18 @@ if type "atuin" >/dev/null; then
     atuin init zsh >>"$gen_zsh"
 fi
 
+if type "aichat" >/dev/null; then
+    cat <<'EOF' >>"$gen_zsh"
+_aichat_zsh() {
+    if [[ -n "$BUFFER" ]]; then
+        local _old=$BUFFER
+        BUFFER+="⌛"
+        zle -I && zle redisplay
+        BUFFER=$(aichat -e "$_old")
+        zle end-of-line
+    fi
+}
+zle -N _aichat_zsh
+bindkey '\ee' _aichat_zsh
+EOF
+fi

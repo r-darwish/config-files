baee322833ef6dd758f795d939276419b3173a35
Roey Darwish Dror <roey.ghost@gmail.com>
2024-08-09 07:48:34 +0300

    Helm templates

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,9 @@
 return {
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.python" },
+  { "towolf/vim-helm" },
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",

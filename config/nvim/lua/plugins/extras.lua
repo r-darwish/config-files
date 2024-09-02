00240543f7be213922e18c2c8026a0a868c3b6ed
Roey Darwish Dror <roey.ghost@gmail.com>
2024-09-02 09:01:48 +0300

    Extras file

---
+++ config/nvim/lua/plugins/extras.lua
@@ -0,0 +1,5 @@
+return {
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.coding.yanky" },
+  { import = "lazyvim.plugins.extras.editor.aerial" },
+}

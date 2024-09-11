4fae39c7207f2e4d03ae14e6bcca5820a4ed7ebe
Roey Darwish Dror <roey.ghost@gmail.com>
2024-09-11 13:13:19 +0300

    Nushell

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -1,6 +1,7 @@
 return {
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
   { import = "lazyvim.plugins.extras.coding.yanky" },
+  { import = "lazyvim.plugins.extras.lang.nushell" },
   { import = "lazyvim.plugins.extras.editor.aerial" },
   { import = "lazyvim.plugins.extras.editor.dial" },
 }

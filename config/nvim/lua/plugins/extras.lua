2b30d6ece79b1b2c0fd4f55b8f3ffbc40a674eca
Roey Darwish Dror <roey.ghost@gmail.com>
2024-09-11 21:43:44 +0300

    Octo and mini files

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -3,4 +3,6 @@ return {
   { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.editor.aerial" },
   { import = "lazyvim.plugins.extras.editor.dial" },
+  { import = "lazyvim.plugins.extras.editor.mini-files" },
+  { import = "lazyvim.plugins.extras.util.octo" },
 }

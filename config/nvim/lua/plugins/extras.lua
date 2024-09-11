0e877762b33756507ce05bad8b4800285660e62d
Roey Darwish Dror <roey.ghost@gmail.com>
2024-09-11 13:25:25 +0300

    More languages

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -1,7 +1,6 @@
 return {
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
   { import = "lazyvim.plugins.extras.coding.yanky" },
-  { import = "lazyvim.plugins.extras.lang.nushell" },
   { import = "lazyvim.plugins.extras.editor.aerial" },
   { import = "lazyvim.plugins.extras.editor.dial" },
 }
--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,9 +1,13 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.nushell" },
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.python" },
-  { "towolf/vim-helm" },
+  { import = "lazyvim.plugins.extras.lang.terraform" },
+  { import = "lazyvim.plugins.extras.lang.helm" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",

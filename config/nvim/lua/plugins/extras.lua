2ac5cd37b35252493eddf441e126b530aa23273c
Roey Darwish Dror <roey.ghost@gmail.com>
2024-09-12 14:07:31 +0300

    More plugins

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -4,5 +4,8 @@ return {
   { import = "lazyvim.plugins.extras.editor.aerial" },
   { import = "lazyvim.plugins.extras.editor.dial" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
+  { import = "lazyvim.plugins.extras.editor.mini-diff" },
+  { import = "lazyvim.plugins.extras.editor.illuminate" },
+  { import = "lazyvim.plugins.extras.editor.navic" },
   { import = "lazyvim.plugins.extras.util.octo" },
 }

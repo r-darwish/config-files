f4962e3e2f1c7e92dd3f09f4da97170c63df1a38
Roey Darwish Dror <roey.ghost@gmail.com>
2024-09-20 11:29:18 +0300

    Theme touchups

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,13 @@
 return {
-  { "diegoulloao/neofusion.nvim", opts = {
-    transparent_mode = true,
-  } },
+  {
+    "diegoulloao/neofusion.nvim",
+    opts = {
+      transparent_mode = true,
+      overrides = {
+        IncSearch = { fg = "#35b5ff" },
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {

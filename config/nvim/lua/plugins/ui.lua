189b2edc9de9abdc5699f4d91777851dbcbac030
Roey Darwish Dror <roey.ghost@gmail.com>
2024-09-26 15:28:43 +0300

    Overseer in Lua line

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,8 +1,14 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      table.remove(opts.sections.lualine_x, 1)
-    end,
+    opts = {
+      sections = {
+        lualine_x = {
+          {
+            "overseer",
+          },
+        },
+      },
+    },
   },
 }

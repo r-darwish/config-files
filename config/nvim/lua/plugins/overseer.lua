6a69cac4ad7a553e838af6662a6b37c932417249
Roey Darwish Dror <roey.ghost@gmail.com>
2024-09-29 16:24:04 +0300

    Restart last task

---
+++ config/nvim/lua/plugins/overseer.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "stevearc/overseer.nvim",
+    keys = {
+      {
+        "<leader>ol",
+        desc = "Restart last task",
+        function()
+          local overseer = require("overseer")
+          local tasks = overseer.list_tasks({ recent_first = true })
+          if vim.tbl_isempty(tasks) then
+            vim.notify("No tasks found", vim.log.levels.WARN)
+          else
+            overseer.run_action(tasks[1], "restart")
+          end
+        end,
+      },
+    },
+  },
+}

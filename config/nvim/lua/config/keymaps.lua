e7126d20f3b9d87e9ae99e22e25eb2d36c1d4b78
Roey Darwish Dror <roey.ghost@gmail.com>
2024-09-30 06:51:44 +0300

    Copy path

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,20 @@ map("n", "<C-f>", ":%s/\\v")
 map("x", "<C-f>", ":s/\\v")
 map({ "n", "x" }, "gh", "^")
 map({ "n", "x" }, "gl", "$")
+
+map({ "n", "x" }, "<leader>fY", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard')
+end, { desc = "Copy absolute path" })
+
+map({ "n", "x" }, "<leader>fy", function()
+  local git = require("gitlinker.git")
+  local root = git.get_git_root() .. "/"
+  local path = vim.fn.expand("%:p")
+
+  path = path:gsub(root, "")
+
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard')
+end, { desc = "Copy absolute path" })

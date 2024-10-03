fd3ffb92ee8c93c5637ce6aa5bd78a7115904109
Roey Darwish Dror <roey.ghost@gmail.com>
2024-10-03 12:27:54 +0300

    Fix history and clipboard

--- config/kitty/history.py
+++ config/kitty/history.py
@@ -1,12 +1,13 @@
 import subprocess
 import sys
 from typing import List
+import os
 from kittens.tui.handler import result_handler  # type: ignore
 from kitty.boss import Boss  # type: ignore
 
 
 def main(args: List[str]) -> None:
-    subprocess.run(["/usr/local/bin/nvim"], stdin=sys.stdin)
+    subprocess.run([os.environ.get("EDITOR", "/usr/local/bin/nvim")], stdin=sys.stdin)
 
 
 @result_handler(type_of_input="history")

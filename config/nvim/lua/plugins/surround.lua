5931766fb771f16926bdcc8d1795e5ceca4b4bca
Roey Darwish Dror <roey.ghost@gmail.com>
2024-10-04 13:47:23 +0300

    Default surround keys

--- config/nvim/lua/plugins/surround.lua
+++ config/nvim/lua/plugins/surround.lua
@@ -1,17 +1,3 @@
 return {
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
-  {
-    "echasnovski/mini.surround",
-    opts = {
-      mappings = {
-        add = ";;",
-        delete = ";d",
-        find = ";f",
-        find_left = ";F",
-        highlight = ";h",
-        replace = ";r",
-        update_n_lines = ";n",
-      },
-    },
-  },
 }

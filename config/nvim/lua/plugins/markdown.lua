fc9f300ccc0664da1cc409777474934f010ea180
Roey Darwish Dror <roey.ghost@gmail.com>
2024-10-04 13:22:28 +0300

    Markdown icons

---
+++ config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = {
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        sign = true,
+      },
+    },
+  },
+}
--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -8,7 +8,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.docker" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
   -- add any tools you want to have installed below
   {
     "neovim/nvim-lspconfig",

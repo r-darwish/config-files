45808e1cb1c6535c45d9ba5376fc7f77fa23d620
Roey Darwish Dror <roey.ghost@gmail.com>
2024-10-05 08:25:57 +0300

    Hipattern instead of colorizer

--- config/nvim/lua/plugins/colorizer.lua
+++ config/nvim/lua/plugins/colorizer.lua
@@ -1,3 +1,24 @@
 return {
-  "norcalli/nvim-colorizer.lua",
+  { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+  { "nvchad/volt", lazy = true },
+  {
+    "nvchad/minty",
+    lazy = true,
+    keys = {
+      {
+        "<leader>cc",
+        function()
+          require("minty.huefy").open({ border = true })
+        end,
+        desc = "Minty Huefy",
+      },
+      {
+        "<leader>cC",
+        function()
+          require("minty.shades").open({ border = true })
+        end,
+        desc = "Minty Shades",
+      },
+    },
+  },
 }

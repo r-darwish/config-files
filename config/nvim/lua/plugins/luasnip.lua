af388df4080e78b190ef1f3fa2860c657e3b3ca0
Roey Darwish Dror <roey.ghost@gmail.com>
2024-10-05 19:49:24 +0300

    Better supertab

--- config/nvim/lua/plugins/luasnip.lua
+++ config/nvim/lua/plugins/luasnip.lua
@@ -1,16 +1,16 @@
 return {
   {
-    "L3MON4D3/LuaSnip",
-    keys = {
-      {
-        "<C-x>",
-        function()
-          local ls = require("luasnip")
-          ls.expand()
-        end,
-        mode = { "i" },
-        remap = true,
-      },
-    },
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      for _, value in ipairs(opts.sources) do
+        if value.name == "luasnip" then
+          value.priority = 100
+          value.group_index = 1
+        end
+      end
+      vim.print(opts.sources)
+    end,
   },
 }

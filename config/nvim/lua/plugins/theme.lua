26a351e98856913971baa3ed24c35da41a46757b
Roey Darwish Dror <roey.ghost@gmail.com>
2024-10-07 22:19:48 +0300

    Keep rose-pine

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,35 +1,11 @@
 return {
   {
-    "diegoulloao/neofusion.nvim",
-    enabled = false,
-    opts = {
-      transparent_mode = true,
-      palette_overrides = {
-        gray = "#008DA3",
-      },
-      overrides = {
-        IncSearch = { fg = "#35b5ff" },
-      },
-    },
-  },
-  {
-    "scottmckendry/cyberdream.nvim",
-    enabled = false,
-    opts = {
-      transparent = true,
-      borderless_telescope = false,
-    },
-  },
-  {
     "rose-pine/neovim",
-    enabled = false,
+    enabled = true,
     name = "rose-pine",
     opts = {
       variant = "moon",
       dark_variant = "moon",
-      styles = {
-        transparency = true,
-      },
     },
   },
   {

99713c52541ace7a56093e3faf1703c49c6050f5
Roey Darwish Dror <roey.ghost@gmail.com>
2024-10-21 11:49:52 +0300

    Better diffview keys

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -1,5 +1,23 @@
 return {
-  { "sindrets/diffview.nvim", keys = {
-    { "<leader>gf", "<cmd>DiffviewFileHistory %<cr>" },
-  } },
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      { "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", desc = "Open File History" },
+      { "<leader>gd", "<cmd>DiffviewOpen <cr>", desc = "Open File Diff" },
+    },
+    opts = {
+      keymaps = {
+        file_panel = {
+          ["gf"] = function()
+            local actions = require("diffview.actions")
+            actions.goto_file()
+            vim.cmd("tabclose #")
+          end,
+          ["q"] = function()
+            vim.cmd("tabclose")
+          end,
+        },
+      },
+    },
+  },
 }

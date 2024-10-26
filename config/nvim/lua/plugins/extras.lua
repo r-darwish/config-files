1149386168d4b786e0d90aee0c917409d6cc2769
Roey Darwish Dror <roey.ghost@gmail.com>
2024-10-26 22:34:48 +0300

    Disable autochdir and use the minifiles extra

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -2,6 +2,7 @@ return {
   { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.coding.luasnip" },
   { import = "lazyvim.plugins.extras.editor.aerial" },
+  { import = "lazyvim.plugins.extras.editor.mini-files" },
   { import = "lazyvim.plugins.extras.editor.dial" },
   { import = "lazyvim.plugins.extras.editor.illuminate" },
   { import = "lazyvim.plugins.extras.editor.navic" },

7a6baecbbbe1b6ee590270ba633fce4e3466be6c
Roey Darwish Dror <roey.ghost@gmail.com>
2024-11-15 17:00:42 +0200

    WSL clipboard

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -10,3 +10,18 @@ vim.opt.titlestring = [[%{v:progname} - %f%h%m%r%w]]
 vim.opt.title = true
 vim.opt.shada = "'1000,<1000,s100"
 vim.opt.cursorline = false
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end

6263c19f0170ee4c626c2e0d335b8d4b06b7f63f
Roey Darwish Dror <roey.ghost@gmail.com>
2024-11-21 19:50:54 +0200

    Neovide stuff

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -34,11 +34,18 @@ end
 if vim.g.neovide then
   vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h14" -- text below applies for VimScript
   vim.g.neovide_input_use_logo = 1
+  vim.g.neovide_cursor_trail_size = 0.6
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_cursor_vfx_particle_density = 10.0
 
-  vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+  vim.api.nvim_set_keymap("t", "<D-v>", "<C-\\><C-n>+pi", { noremap = true, silent = true })
 end
 
 vim.api.nvim_create_user_command("ChdirFile", function()

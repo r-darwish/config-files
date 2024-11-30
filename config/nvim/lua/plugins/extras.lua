464e6d97bf257e005f80ac66ccabf9b334dbae2d
Roey Darwish Dror <roey.ghost@gmail.com>
2024-11-30 08:50:06 +0200

    Replace toggleterm

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -4,7 +4,8 @@ return {
   { import = "lazyvim.plugins.extras.editor.illuminate" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
   { import = "lazyvim.plugins.extras.editor.navic" },
-  { import = "lazyvim.plugins.extras.editor.refactoring" },
+  { import = "lazyvim.plugins.extras.editor.navic" },
+  { import = "lazyvim.plugins.extras.ui.edgy" },
   { import = "lazyvim.plugins.extras.test" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = "lazyvim.plugins.extras.util.octo" },
@@ -21,6 +22,9 @@ return {
         width_nofocus = 25,
         width_preview = 100,
       },
+    },
+    mappings = {
+      go_in_plus = "<CR>",
     },
   },
 }

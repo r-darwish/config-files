debeb336d9a930d02442947b730fb95f4b532d51
Roey Darwish Dror <roey.ghost@gmail.com>
2024-11-30 20:11:40 +0200

    Set Telescope to vertical

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -22,7 +22,7 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     opts = function(_, opts)
-      opts.defaults.layout_strategy = "flex"
+      opts.defaults.layout_strategy = "vertical"
     end,
     keys = {
       {

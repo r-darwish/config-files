aa9bf8d2580308d1b1670171f0f839960d041fd7
Roey Darwish Dror <roey.ghost@gmail.com>
2024-12-18 18:58:16 +0200

    Remove edgy

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -1,5 +1,4 @@
 return {
-  { import = "lazyvim.plugins.extras.ui.edgy" },
   { import = "lazyvim.plugins.extras.coding.luasnip" },
   { import = "lazyvim.plugins.extras.editor.aerial" },
   { import = "lazyvim.plugins.extras.editor.illuminate" },

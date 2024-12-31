761acf9ea0a4b231f770a77c0a3e94bd91e03784
Roey Darwish Dror <roey.ghost@gmail.com>
2024-12-31 17:03:00 +0200

    Float copilot

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -2,6 +2,16 @@ return {
   { import = "lazyvim.plugins.extras.ai.copilot" },
   { import = "lazyvim.plugins.extras.ai.copilot-chat" },
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      window = {
+        layout = "float",
+        width = 0.75,
+        height = 0.75,
+      },
+    },
+  },
+  {
     "zbirenbaum/copilot.lua",
     keys = {
       { "<leader>at", "<cmd>Copilot toggle<CR>" },

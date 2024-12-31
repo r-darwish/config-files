84d92aa810ae287ae8a49b3b845cbdf22be4453a
Roey Darwish Dror <roey.ghost@gmail.com>
2024-12-31 13:43:43 +0200

    Harpoon

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -4,6 +4,7 @@ return {
   { import = "lazyvim.plugins.extras.editor.illuminate" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
   { import = "lazyvim.plugins.extras.editor.navic" },
+  { import = "lazyvim.plugins.extras.editor.harpoon2" },
   { import = "lazyvim.plugins.extras.test" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   {

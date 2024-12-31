58282067c2567cdc6b8809a0610dbcf9f036617d
Roey Darwish Dror <roey.ghost@gmail.com>
2024-12-31 13:43:51 +0200

    Change <leader>fn

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -96,3 +96,10 @@ map({ "n", "x" }, "<leader>gu", function()
     end
   end)
 end, { desc = "Pull" })
+
+local function open_file_in_same_dir()
+  local current_file = vim.fn.expand("%:p:h")
+  local file_to_open = vim.fn.input("Enter file name: ", current_file .. "/", "file")
+  vim.cmd("edit " .. file_to_open)
+end
+map({ "n", "v" }, "<leader>fn", open_file_in_same_dir)

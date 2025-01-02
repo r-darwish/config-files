6c02e0e25c4d3356222256788f5aad831d381f12
Roey Darwish Dror <roey.ghost@gmail.com>
2025-01-02 12:44:22 +0200

    Disable blink in markdown files

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,11 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    enabled = function()
+      return not vim.tbl_contains({ "markdown" }, vim.bo.filetype)
+        and vim.bo.buftype ~= "prompt"
+        and vim.b.completion ~= false
+    end,
     completion = {
       accept = {
         auto_brackets = {

2b31a16281cde321176110ba7900ee4343ef646c
Roey Darwish Dror <roey.ghost@gmail.com>
2025-01-07 11:43:24 +0200

    Hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,13 @@
+local HyperShortcuts = {
+	{ "S", "Slack" },
+	{ "F", "Firefox" },
+	{ "T", "Kitty" },
+	{ "W", "WhatsApp" },
+	{ "M", "Spotify" },
+}
+
+for _, shortcut in ipairs(HyperShortcuts) do
+	hs.hotkey.bind({ "ctrl", "alt", "shift", "cmd" }, shortcut[1], function()
+		hs.application.launchOrFocus(shortcut[2])
+	end)
+end

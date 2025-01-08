220e6133a4f0ad2dd26e378128802cfce2eceda2
Roey Darwish Dror <roey.ghost@gmail.com>
2025-01-08 09:17:00 +0200

    Remove the markdown lint

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -4,6 +4,24 @@ return {
     "jghauser/follow-md-links.nvim",
   },
   {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = {},
+      },
+    },
+  },
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
       heading = {

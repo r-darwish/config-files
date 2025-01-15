c56d369c7d7d734bee351f0f000b3e50c4db4185
Roey Darwish Dror <roey.ghost@gmail.com>
2025-01-15 20:16:21 +0200

    No need to luasnip

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -1,5 +1,4 @@
 return {
-  { import = "lazyvim.plugins.extras.coding.luasnip" },
   { import = "lazyvim.plugins.extras.editor.aerial" },
   { import = "lazyvim.plugins.extras.editor.illuminate" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },

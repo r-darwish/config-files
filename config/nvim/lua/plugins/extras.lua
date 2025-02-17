1091a7910dcea461eb5d4f11788e5ff0d4bfb6c1
Roey Darwish Dror <roey.ghost@gmail.com>
2025-02-17 09:05:57 +0200

    Add the dot extra

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -5,6 +5,7 @@ return {
   { import = "lazyvim.plugins.extras.editor.navic" },
   { import = "lazyvim.plugins.extras.test" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.util.dot" },
   {
     "linux-cultist/venv-selector.nvim",
     opts = {

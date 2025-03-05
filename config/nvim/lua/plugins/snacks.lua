39f66ebc936b6a52b6fb157b626adb7534e84011
Roey Darwish Dror <roey.ghost@gmail.com>
2025-03-05 08:31:14 +0200

    Close the picker with esc

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,15 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<Esc>"] = { "close", mode = { "n", "i" } },
+            },
+          },
+        },
+      },
       dashboard = {
         enabled = false,
         sections = {

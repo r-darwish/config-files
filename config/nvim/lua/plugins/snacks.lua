a34a8e01257a087d45ea160d5c1e2410b15e759b
Roey Darwish Dror <roey.ghost@gmail.com>
2025-03-05 08:53:20 +0200

    Restore the dashboard

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,7 @@
 return {
   {
     "folke/snacks.nvim",
+    ---@type snacks.Config
     opts = {
       picker = {
         win = {
@@ -9,26 +10,6 @@ return {
               ["<Esc>"] = { "close", mode = { "n", "i" } },
             },
           },
-        },
-      },
-      dashboard = {
-        enabled = false,
-        sections = {
-          { pane = 1, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-          {
-            pane = 1,
-            icon = " ",
-            title = "Git Status",
-            section = "terminal",
-            enabled = vim.fn.isdirectory(".git") == 1,
-            cmd = "hub status --short --branch --renames",
-            height = 5,
-            padding = 1,
-            ttl = 5 * 60,
-            indent = 3,
-          },
-          { section = "startup" },
         },
       },
     },

1062a852b7bce9a436322ca49487e9dc710d8168
Roey Darwish Dror <roey.ghost@gmail.com>
2025-03-07 14:30:13 +0200

    Set up paste keys in Windows Neovide

--- config/nvim/lua/darwish/neovide.lua
+++ config/nvim/lua/darwish/neovide.lua
@@ -1,10 +1,19 @@
-vim.keymap.set({ "n", "v" }, "<D-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
-vim.keymap.set({ "n", "v" }, "<D-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
-vim.keymap.set({ "n", "v" }, "<D-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+local macos = vim.fn.has("macunix") == 1
+local paste_key = macos and "<D-v>" or "<S-Insert>"
+
+if macos then
+  vim.keymap.set({ "n", "v" }, "<D-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+  vim.keymap.set({ "n", "v" }, "<D-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+  vim.keymap.set({ "n", "v" }, "<D-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+else
+  vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+  vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+  vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+end
 
 vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
 vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-vim.keymap.set("n", "<D-v>", '"+p') -- Paste normal mode
-vim.keymap.set("v", "<D-v>", '"+p') -- Paste visual mode
-vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-vim.keymap.set("i", "<D-v>", '<ESC>"+pi') -- Paste insert mode
+vim.keymap.set("n", paste_key, '"+p') -- Paste normal mode
+vim.keymap.set("v", paste_key, '"+p') -- Paste visual mode
+vim.keymap.set("c", paste_key, "<C-R>+") -- Paste command mode
+vim.keymap.set("i", paste_key, '<ESC>"+pi') -- Paste insert mode

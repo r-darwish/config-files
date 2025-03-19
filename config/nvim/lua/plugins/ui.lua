0c92bbf5ce0d85a672bf551e662c6357edda53ff
Roey Darwish Dror <roey.ghost@gmail.com>
2025-03-19 08:38:26 +0200

    Show Linux

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -8,6 +8,32 @@ local function lint_checker()
   return string.format("%s 󰑮 %s", "%#Debug#", table.concat(linters, ", "))
 end
 
+---@type string?
+local _linux_name = nil
+
+---Return the name of the Linux system
+---@return string
+local function linux_name()
+  if not vim.fn.has("linux") then
+    return ""
+  end
+
+  if _linux_name == nil then
+    _linux_name = (function()
+      local wsl_distro = vim.fn.getenv("WSL_DISTRO_NAME")
+      if wsl_distro ~= nil then
+        return wsl_distro
+      end
+
+      if vim.fn.getenv("SSH_CONNECTION") ~= nil then
+        return vim.system({ "hostname" }, { text = true }):wait().stdout
+      end
+    end)()
+  end
+
+  return string.format("%s  %s", "%#WarningMsg#", _linux_name)
+end
+
 ---Check if the LSP is ready
 ---@return string
 local function lsp_checker()
@@ -30,6 +56,7 @@ return {
       })
       table.insert(opts.sections.lualine_x, lint_checker)
       table.insert(opts.sections.lualine_x, lsp_checker)
+      table.insert(opts.sections.lualine_c, 1, linux_name)
       opts.sections.lualine_c[4] = { require("lazyvim.util.lualine").pretty_path({ relative = "root", length = 10 }) }
       opts.options.section_separators = { left = "", right = "" }
       opts.options.section_separators.component_separators = { left = "", right = "" }

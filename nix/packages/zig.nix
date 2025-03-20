3a3b2efe68233e751bf83d88383249d4418fdbf3
Roey Darwish Dror <roey.ghost@gmail.com>
2025-03-20 13:15:39 +0000

    Install Zig from unstable

--- nix/packages/zig.nix
+++ nix/packages/zig.nix
@@ -1,8 +1,11 @@
 { pkgs, lib, config, ... }:
 with lib;
-let cfg = config.darwish.zig;
+let
+  cfg = config.darwish.zig;
+  pkgsUnstable = import <nixpkgs-unstable> { };
 in {
   options.darwish.zig = { enable = mkEnableOption "Zig packages"; };
 
-  config = mkIf cfg.enable { home.packages = with pkgs; [ zig zls ]; };
+  config =
+    mkIf cfg.enable { home.packages = with pkgs; [ pkgsUnstable.zig zls ]; };
 }

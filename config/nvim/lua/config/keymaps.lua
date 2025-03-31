2c35b2866b0acf6559842973089d51f31d325afb
Roey Darwish Dror <roey.ghost@gmail.com>
2025-03-31 09:20:37 +0300

    Fold keys

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -24,6 +24,8 @@ map({ "i" }, "<C-z>", "<esc>")
 
 map({ "n", "x" }, "gh", "^", { desc = "Line start" })
 map({ "n", "x" }, "gl", "$", { desc = "Line end" })
+map({ "n", "x" }, "<C-->", "zm", { desc = "Fold more" })
+map({ "n", "x" }, "<C-=>", "zr", { desc = "Fold less" })
 map({ "n", "x" }, "<leader>wb", "<C-w>T", { desc = "Break the current window to a new tab" })
 map({ "n" }, "gp", "`[v`]", { desc = "Last paste" })
 map({ "n", "x" }, "q:", "<nop>")

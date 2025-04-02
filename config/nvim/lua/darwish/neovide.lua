c0f01c63f9fe4a5d0b9501270e61501e423f1b36
Roey Darwish Dror <roey.ghost@gmail.com>
2025-04-02 13:10:30 +0300

    Fix terminal key bindings
    Fix keys

--- config/nvim/lua/darwish/neovide.lua
+++ config/nvim/lua/darwish/neovide.lua
@@ -2,22 +2,21 @@ local macos = vim.fn.has("macunix") == 1
 local paste_key = macos and "<D-v>" or "<S-Insert>"
 
 if macos then
-  vim.keymap.set({ "n", "v" }, "<D-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
-  vim.keymap.set({ "n", "v" }, "<D-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
-  vim.keymap.set({ "n", "v" }, "<D-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+  vim.keymap.set({ "n", "x" }, "<D-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+  vim.keymap.set({ "n", "x" }, "<D-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+  vim.keymap.set({ "n", "x" }, "<D-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
 else
-  vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
-  vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
-  vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+  vim.keymap.set({ "n", "x" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+  vim.keymap.set({ "n", "x" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+  vim.keymap.set({ "n", "x" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
 end
 
-vim.keymap.set({ "n", "v" }, "<D-t>", "<C-w>T")
-vim.keymap.set({ "n", "v" }, "<C-tab>", "gt")
+vim.keymap.set({ "n", "x" }, "<D-t>", "<C-w>T")
+vim.keymap.set({ "n", "x" }, "<D-d>", "<C-w>d")
+vim.keymap.set({ "n", "x" }, "<C-tab>", "gt")
 vim.keymap.set({ "t" }, "<C-tab>", "<C-\\><C-n>gt")
-vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
-vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-vim.keymap.set("n", paste_key, '"+p') -- Paste normal mode
-vim.keymap.set("v", paste_key, '"+p') -- Paste visual mode
-vim.keymap.set("c", paste_key, "<C-R>+") -- Paste command mode
-vim.keymap.set("i", paste_key, '<ESC>"+pi') -- Paste insert mode
+vim.keymap.set("n", "<D-s>", ":w<CR>")
+vim.keymap.set("x", "<D-c>", '"+y')
+vim.keymap.set({ "n", "x" }, paste_key, '"+p')
+vim.keymap.set({ "c", "i" }, paste_key, "<C-R>+")
 vim.keymap.set("t", paste_key, '<C-\\><C-n>"+pi', { noremap = true, silent = true })

7656afd8c0c0c3b1a5b40c76d33d509952060f7b
Roey Darwish Dror <roey.ghost@gmail.com>
2025-04-03 06:37:12 +0300

    Refactor keys

--- config/nvim/lua/darwish/neovide.lua
+++ config/nvim/lua/darwish/neovide.lua
@@ -1,14 +1,35 @@
 local macos = vim.fn.has("macunix") == 1
 local paste_key = macos and "<D-v>" or "<S-Insert>"
 
+--- Change the scale factor
+---@param scale_factor number scale factor
+local function change_scale(scale_factor)
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + scale_factor
+end
+
+--- Increase the scale
+local function increase_scale()
+  change_scale(0.1)
+end
+
+--- Decrease the scale
+local function decrease_scale()
+  change_scale(-0.1)
+end
+
+--- Reset the scale
+local function reset_scale()
+  vim.g.neovide_scale_factor = 1
+end
+
 if macos then
-  vim.keymap.set({ "n", "x" }, "<D-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
-  vim.keymap.set({ "n", "x" }, "<D-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
-  vim.keymap.set({ "n", "x" }, "<D-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+  vim.keymap.set({ "n", "x" }, "<D-=>", increase_scale)
+  vim.keymap.set({ "n", "x" }, "<D-->", decrease_scale)
+  vim.keymap.set({ "n", "x" }, "<D-0>", reset_scale)
 else
-  vim.keymap.set({ "n", "x" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
-  vim.keymap.set({ "n", "x" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
-  vim.keymap.set({ "n", "x" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+  vim.keymap.set({ "n", "x" }, "<C-=>", increase_scale)
+  vim.keymap.set({ "n", "x" }, "<C-->", decrease_scale)
+  vim.keymap.set({ "n", "x" }, "<C-0>", reset_scale)
 end
 
 vim.keymap.set({ "n", "x" }, "<D-t>", "<C-w>T")

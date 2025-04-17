4e2b78fd84fc77cb4b676991519ce295d8cd393c
Roey Darwish Dror <roey.ghost@gmail.com>
2025-04-17 19:14:52 +0300

    No auto formatting for bash

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -24,13 +24,21 @@ vim.api.nvim_create_autocmd({
   pattern = { "*.yaml", "*.yml" },
   group = autocmds.augroup,
   callback = function()
-    -- Disable auto formatting by default
-    vim.b.autoformat = false
-
-    -- If the file contains a go template, set the filetype to helm
     if vim.fn.search("{{.\\+}}", "nw") ~= 0 then
       local buf = vim.api.nvim_get_current_buf()
       vim.api.nvim_set_option_value("filetype", "helm", { buf = buf })
     end
   end,
 })
+
+-- Disable auto format
+vim.api.nvim_create_autocmd({
+    "BufNewFile",
+    "BufRead",
+}, {
+    pattern = { "*.yaml", "*.yml", "*.sh", "*.bash" },
+    group = autocmds.augroup,
+    callback = function()
+      vim.b.autoformat = false
+  end,
+  })

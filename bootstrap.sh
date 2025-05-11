cf5082ff20b886d5969412d58ec91b617c07b0be
Roey Darwish Dror <roey.ghost@gmail.com>
2025-05-11 12:37:58 +0000

    Make bootstrap.sh idempontent

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,7 +22,7 @@ fi
 for c in config/*; do
     link_config "$c"
 done
-ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
+test -f "$HOME/.tmux.conf" || ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
 
 for c in dynamic_config/*; do
     cp -r "$c" ~/.config

d6ceef3556afc16ca86593c52bf53c0459fd18b6
Roey Darwish Dror <roey.ghost@gmail.com>
2025-06-08 08:59:38 +0300

    Flash configuration

---
+++ config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,17 @@
+---@module "flash"
+
+return {
+  "folke/flash.nvim",
+  ---@type Flash.Config
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    label = {
+      rainbow = { enabled = true },
+    },
+    modes = {
+      char = {
+        jump_labels = true,
+      },
+    },
+  },
+}

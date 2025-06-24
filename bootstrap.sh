9fb7fb2c77fab3c992bc4b76287a95bf2c7728ab
Roey Darwish Dror <roey.ghost@gmail.com>
2025-06-24 15:57:06 +0300

    Get the function from common

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,26 @@
 
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
-source "common.zsh"
+
+link_config() {
+    local source target basename
+    source="$PWD/$1"
+    basename=$(basename "$1")
+    target="$HOME/.config/$basename"
+    if [[ $basename == "nushell" && $(uname) == "Darwin" ]]; then
+      target="$HOME/Library/Application Support/$basename"
+    fi
+
+    if link=$(readlink "$target"); then
+        if [[ "$link" == "$source" ]]; then
+            echo "Already linked $target"
+            return 0
+        fi
+    fi
+
+    rm -rf "$target"
+    ln -s "$source" "$target"
+}
 
 mkdir -p ~/.config
 

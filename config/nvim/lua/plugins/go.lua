792e05294f3d64d62c057c809e90a1d4802eb094
Roey Darwish Dror <roey.ghost@gmail.com>
2025-07-15 11:38:33 +0300

    Avoid capturing output when running go tests

--- config/nvim/lua/plugins/go.lua
+++ config/nvim/lua/plugins/go.lua
@@ -1,8 +1,28 @@
 return {
-  "mfussenegger/nvim-lint",
-  opts = {
-    linters_by_ft = {
-      go = { "golangcilint" },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "fredrikaverpil/neotest-golang",
+    },
+    opts = {
+      adapters = {
+        ["neotest-golang"] = {
+          dap_go_enabled = true,
+          go_test_args = {
+            "-race",
+            "-count=1",
+          },
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        go = { "golangcilint" },
+      },
     },
   },
 }

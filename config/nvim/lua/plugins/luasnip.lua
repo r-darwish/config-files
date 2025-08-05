9bc75f22371a26bc68ceffbc12c491e2ed520778
Roey Darwish Dror <roey.ghost@gmail.com>
2025-08-05 22:31:52 +0300

    Snippets

--- config/nvim/lua/plugins/luasnip.lua
+++ config/nvim/lua/plugins/luasnip.lua
@@ -1,7 +1,7 @@
 return {
   "L3MON4D3/LuaSnip",
   opts = function()
-    require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/LuaSnip/" } })
+    require("luasnip.loaders.from_vscode").load({ paths = { "./snippets" } })
   end,
   keys = {
     {

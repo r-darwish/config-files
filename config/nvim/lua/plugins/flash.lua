0ff09764ba635399fee86d87ccce16a08d966e6c
Roey Darwish Dror <roey.ghost@gmail.com>
2025-08-06 09:06:30 +0300

    Enable rainbow

--- config/nvim/lua/plugins/flash.lua
+++ config/nvim/lua/plugins/flash.lua
@@ -5,6 +5,11 @@ return {
   ---@type Flash.Config
   ---@diagnostic disable-next-line: missing-fields
   opts = {
+    label = {
+      rainbow = {
+        enabled = true,
+      },
+    },
     modes = {
       char = {
         jump_labels = false,

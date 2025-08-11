61f2278a24a1c7511f0e6b55edbc96652d7412b0
Roey Darwish Dror <roey.ghost@gmail.com>
2025-08-11 09:36:42 +0300

    Install NPM

--- bootstrap.sh
+++ bootstrap.sh
@@ -66,7 +66,7 @@ if type "brew" >/dev/null 2>&1; then
     if [[ -n "$BACKGROUND" ]]; then
         export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
         brew tap rsteube/homebrew-tap || true
-        brew install fish starship atuin zoxide neovim vivid lsd || true
+        brew install fish npm starship atuin zoxide neovim vivid lsd || true
         if [[ "$(uname)" == "Linux" ]]; then
             echo "exec /home/linuxbrew/.linuxbrew/bin/fish" > ~/.zshrc
         fi
@@ -83,7 +83,7 @@ if [[ -d "$HOME/wiz-sec" ]]; then
 fi
 
 if type "pacman" >/dev/null 2>&1; then
-    sudo pacman -S --needed atuin base-devel fd fzf git github-cli go htop lazygit neovim fish python ripgrep starship tmux unzip uv yazi zoxide zellij vivid ghostty-terminfo gum lsd
+    sudo pacman -S --needed atuin base-devel fd fzf git github-cli go htop lazygit neovim nodejs npm fish python ripgrep starship tmux unzip uv yazi zoxide zellij vivid ghostty-terminfo gum lsd
 fi
 
 ./bootstrap.fish

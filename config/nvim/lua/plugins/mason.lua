65d09ac6a9b3790563c376c5a9cb1fdff6b0269f
Roey Darwish Dror <roey.ghost@gmail.com>
2025-08-12 22:31:41 +0300

    Install stuff based if depdendencies are present

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,21 @@
 local enable_mason = not require("darwish.utils").path_exists("~/.nix-profile")
 
+local app_deps = {
+  goimports = "go",
+  gofumpt = "go",
+  delve = "go",
+  gci = "go",
+  gotestsum = "go",
+}
+
+--- returns whether an app should be installed
+---@param app string
+local function should_install(app)
+  local dep = app_deps[app]
+
+  return dep == nil or vim.fn.executable(dep) == 1
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -14,16 +30,28 @@ return {
     cond = enable_mason,
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     cond = enable_mason,
-    opts = {
-      ensure_installed = {
+    opts = function(_, opts)
+      for _, app in ipairs({
         "shellcheck",
         "shfmt",
         "bash-language-server",
         "gci",
         "gotestsum",
-      },
-    },
+      }) do
+        table.insert(opts.ensure_installed, app)
+      end
+
+      for i, app in ipairs(opts.ensure_installed) do
+        if not should_install(app) then
+          local last = #opts.ensure_installed
+          opts.ensure_installed[i] = opts.ensure_installed[last]
+          opts.ensure_installed[last] = nil
+        end
+      end
+
+      return opts
+    end,
   },
 }

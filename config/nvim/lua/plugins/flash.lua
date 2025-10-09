97df34b4f8990609fa73b4f94b566e6513ff11ac
Roey Darwish Dror <roey.ghost@gmail.com>
2025-10-09 12:05:16 +0300

    Change incremental selection labels

--- config/nvim/lua/plugins/flash.lua
+++ config/nvim/lua/plugins/flash.lua
@@ -4,6 +4,22 @@ return {
   "folke/flash.nvim",
   ---@type Flash.Config
   ---@diagnostic disable-next-line: missing-fields
+  keys = {
+    {
+      "<c-space>",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter({
+          labels = "123456789",
+          actions = {
+            ["<c-space>"] = "next",
+            ["<BS>"] = "prev",
+          },
+        })
+      end,
+      desc = "Treesitter Incremental Selection",
+    },
+  },
   opts = {
     label = {
       rainbow = {

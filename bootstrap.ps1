d0acf95bb27c5eb3bc8b151e71ec23177955f962
Roey Darwish Dror <roey.ghost@gmail.com>
2025-11-11 06:46:06 +0200

    Install double commander and the sysinternals suite

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,4 @@
-winget install microsoft.powershell Git.Git starship neovim junegunn.fzf lazygit OpenJS.NodeJS peazip yazi sharkdp.fd ripgrep python3 zoxide eza-community.eza dandavison.delta
+winget install microsoft.powershell Git.Git starship neovim junegunn.fzf lazygit OpenJS.NodeJS peazip yazi sharkdp.fd ripgrep python3 zoxide eza-community.eza dandavison.delta alexx2000.DoubleCommander Microsoft.Sysinternals.Suite
 
 New-Item -ItemType SymbolicLink -Path "~\.gitconfig" -Target "$pwd\.gitconfig"
 git config --global core.sshCommand 'C:\\Windows\\System32\\OpenSSH\\ssh.exe'

80bbfc8f66693450705ec07565d526c0bfe6490f
Roey Darwish Dror <roey.ghost@gmail.com>
2025-11-15 08:47:09 +0200

    Install some modules

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,3 +2,6 @@ winget install microsoft.powershell Git.Git starship neovim junegunn.fzf lazygit
 
 New-Item -ItemType SymbolicLink -Path "~\.gitconfig" -Target "$pwd\.gitconfig"
 git config --global core.sshCommand 'C:\\Windows\\System32\\OpenSSH\\ssh.exe'
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+pwsh -c "Install-Module PSFzf,Microsoft.WinGet.Client,Microsoft.Powershell.ConsoleGuiTools"
